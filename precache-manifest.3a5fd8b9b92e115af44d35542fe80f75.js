1eb7a74a155f40a3a8dbbead99c3b5b4ca0df43d
avdeevaelena5@gmail.com <avdeevaelena5@gmail.com>
2020-04-18 20:03:43 +0300

    Updates

---
+++ precache-manifest.3a5fd8b9b92e115af44d35542fe80f75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0ee51bcc9c4d2bcab2d253d3945dbf",
+    "url": "/OscarReact/index.html"
+  },
+  {
+    "revision": "c1e81d555275cfb0dd0d",
+    "url": "/OscarReact/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "5ba972a7e69f8c32351c",
+    "url": "/OscarReact/static/css/main.2286f01c.chunk.css"
+  },
+  {
+    "revision": "c1e81d555275cfb0dd0d",
+    "url": "/OscarReact/static/js/2.f1deb22c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/OscarReact/static/js/2.f1deb22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba972a7e69f8c32351c",
+    "url": "/OscarReact/static/js/main.f055000d.chunk.js"
+  },
+  {
+    "revision": "bbd42a0083477743f9b1",
+    "url": "/OscarReact/static/js/runtime-main.42b3e998.js"
+  },
+  {
+    "revision": "94bac9cba0000e3200e67bc9dd711e4d",
+    "url": "/OscarReact/static/media/oscar.94bac9cb.png"
+  }
+]);
